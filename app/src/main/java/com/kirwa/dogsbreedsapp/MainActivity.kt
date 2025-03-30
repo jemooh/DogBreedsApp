7d454e6ab5f850b7b3ad1c8fbd59e699f1c2c450
James Kirwa <james.kirwa@twiga.com>
2025-03-30 15:44:18 +0300

    - Initial commit

---
+++ app/src/main/java/com/kirwa/dogsbreedsapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.kirwa.dogsbreedsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kirwa.dogsbreedsapp.ui.theme.DogsBreedsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DogsBreedsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DogsBreedsAppTheme {
+        Greeting("Android")
+    }
+}
